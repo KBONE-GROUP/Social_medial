4b720ab6c42d009bd96ba8c87252ca4e1b0b5a8f
Jacky <108917622+Jacky-Homes@users.noreply.github.com>
2022-12-07 22:38:42 +0900

    logout

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('signup/', views.signup, name="signup")
+    path('signup/', views.signup, name="signup"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('logout/', views.logout_view, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,10 +4,37 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from .models import Profile
+from django.contrib.auth import logout
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def index(request):
-  return render(request, 'signin.html')
+  # if request.user.is_authenticated:
+  #     return redirect('dashboard')
+  if request.method == "POST":
+      username = request.POST['username']
+      password = request.POST['password']
+      
+      user = auth.authenticate(username=username, password=password)
+      
+      request.session['username'] = username
+      if user is not None:
+        auth.login(request, user)
+        return redirect('/dashboard')
+      else:
+        messages.info(request, "Credentials Invalid")
+        return redirect('/')
+  else:
+    return render(request, 'signin.html')
+  
+  form = AuthenticationForm()
+  
+  # return render(
+  #     request=request,
+  #     template_name="users/login.html", 
+  #     context={'form': form}
+  #     )
 def signup(request):
   if request.method == "POST":
     username = request.POST['username']
@@ -21,7 +48,7 @@ def signup(request):
         return redirect('signup')
       elif User.objects.filter(username=username).exists():
         messages.info(request, 'Username Taken')
-        return redirecet('signup')
+        return redirect('signup')
       else:
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
@@ -36,3 +63,16 @@ def signup(request):
       messages.info(request, 'Password Not Matching')
       return redirect('signup')
   return render(request, 'signup.html')
+
+def dashboard(request):
+  user = getattr(request, "user", None)
+  is_authenticated = getattr(user, "is_authenticated", True)
+  if is_authenticated == True:
+    return render(request, 'dashboard.html')
+  return redirect('/')
+
+@csrf_exempt
+def logout_view(request):
+  logout(request)
+  messages.info(request, "Logged out successfully!")
+  return redirect('/')
